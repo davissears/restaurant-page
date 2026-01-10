36ed2b48e34e3585dad704df964472cf890d49d6
davissears <davisrandsears@gmail.com>
2026-01-09 16:15:52 -0800

    Add background image to content home

--- src/components/contentHome.js
+++ src/components/contentHome.js
@@ -1,8 +1,15 @@
+import homeBg from "../../images/homeBackground.jpg";
+import aboutBg from "../../images/aboutBackground.jpg";
+
 // loads home page content
 export const _Content = (() => {
   // define cotainer for content
   const contentContainer = document.getElementById("content");
   const test = document.createElement("p");
-  test.textContent = "HelloWorld";
-  contentContainer.appendChild(test);
+  // test.textContent = "HelloWorld";
+  // contentContainer.appendChild(test);
+
+  document.body.style.backgroundImage = `url(${homeBg})`;
+  document.body.style.backgroundImage = `url(${homeBg})`;
+  document.body.style.backgroundSize = "cover"; // or "contain", or "100% 100%"
 })();
