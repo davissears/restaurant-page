12fca13c1a0e0e29605bfe351e55142cdb56c0ca
davissears <davisrandsears@gmail.com>
2026-01-09 17:24:51 -0800

    Add background images to home page
    Set the background image for the home page to `homeBackground.jpg` and
    ensure it covers the entire background.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 // imports
 //  contentHome
 import { _Content } from "./components/contentHome";
+import homeBg from "../images/homeBackground.jpg";
+import aboutBg from "../images/aboutBackground.jpg";
+
+document.body.style.backgroundImage = `url(${homeBg})`;
+document.body.style.backgroundImage = `url(${homeBg})`;
+document.body.style.backgroundSize = "cover";
