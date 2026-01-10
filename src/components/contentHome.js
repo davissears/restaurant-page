0dd5a27f2c5629ce5245cdbb92ac40547b3c2646
davissears <davisrandsears@gmail.com>
2026-01-09 17:24:16 -0800

    Update contentHome.js

--- src/components/contentHome.js
+++ src/components/contentHome.js
@@ -1,16 +1,7 @@
-import homeBg from "../../images/homeBackground.jpg";
-import aboutBg from "../../images/aboutBackground.jpg";
-
 // loads home page content
 export const _Content = (() => {
-  // define cotainer for content
-  const contentContainer = document.getElementById("content");
-  const test = document.createElement("p");
-  document.body.style.backgroundImage = `url(${homeBg})`;
-  document.body.style.backgroundImage = `url(${homeBg})`;
-  document.body.style.backgroundSize = "cover";
   class Component {
-    constructor(tag, className, text = '') {
+    constructor(tag, className, text = "") {
       this.element = document.createElement(tag);
       this.element.className = className;
       this.element.textContent = text;
