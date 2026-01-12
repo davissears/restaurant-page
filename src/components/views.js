5fd4bd309e9e530497e048815c35f594a9ef46d7
davissears <davisrandsears@gmail.com>
2026-01-11 17:14:16 -0800

    Refactor views to use a data structure
    restructures the `views.js` file to use a single `viewsData` object that
    holds configuration for different views.

--- src/components/views.js
+++ src/components/views.js
@@ -1,24 +1,94 @@
-// creates collections of view objects for:
-// -home -about -menu
-
-// each view needs:
-//  Background: file, view
-//  PageContent: text, view
-
 // imports
-import { Background, PageContent } from "./content.js";
 import homeBg from "../../images/homeBackground.jpg";
+import aboutBg from "../../images/aboutBackground.jpg";
+import menuBg from "../../images/menuBackground.jpg";
 
-// home Content
-export const home = () => {
-  const homeView = [];
-  //  home Background
-  const homeBackground = new Background(homeBg, "home");
-  homeView.push(homeBackground);
-  homeBackground.changeBg();
-  const homeHeader = "we serve food to people";
-  const homeBody = "Be our guest!";
-  const homeContent = new PageContent(homeHeader, homeBody, "home");
-  homeView.push(homeContent);
-  homeContent.updatePageContent();
+export const viewsData = {
+  home: {
+    bgImage: homeBg,
+    header: "we serve food to people",
+    body: "Be our guest!",
+  },
+  about: {
+    bgImage: aboutBg,
+    header: "A Place for People to eat food",
+    body: "Be our guest! Eat our food! Drink our libations!",
+  },
+  menu: {
+    bgImage: menuBg,
+    items: [
+      {
+        dish: "fries",
+        course: "starter",
+        description: "fried long-cut potatoes, duck-fat, tomato",
+        price: "10",
+        gf: true,
+        vegan: false,
+        vegetarian: false,
+      },
+      {
+        dish: "cesar salad",
+        course: "starter",
+        description: "greens, anchovy, egg",
+        price: "14",
+        gf: false,
+        vegan: false,
+        vegetarian: false,
+      },
+      {
+        dish: "plantains",
+        course: "starter",
+        description: "fried plantains, sherry vinegar, beer-batter",
+        price: "12",
+        gf: false,
+        vegan: true,
+        vegetarian: true,
+      },
+      {
+        dish: "steak",
+        course: "main",
+        description: "ribeye, rosemary, truffle-butter",
+        price: "32",
+        gf: true,
+        vegan: false,
+        vegetarian: false,
+      },
+      {
+        dish: "salmon",
+        course: "main",
+        description: "wild fillet, chimichuri, gremolata",
+        price: "28",
+        gf: true,
+        vegan: false,
+        vegetarian: false,
+      },
+      {
+        dish: "squash",
+        course: "main",
+        description: "butternut, baking spice, pastry",
+        price: "22",
+        gf: false,
+        vegan: true,
+        vegetarian: true,
+      },
+      {
+        dish: "cake",
+        course: "sweets",
+        description: "rum, garam masalla, corn",
+        price: "12",
+        gf: true,
+        vegan: true,
+        vegetarian: true,
+      },
+      {
+        dish: "cashew ice cream",
+        course: "sweets",
+        description: "cashew cream, vanilla, shortbread",
+        price: "10",
+        gf: false,
+        vegan: true,
+        vegetarian: true,
+      },
+    ],
+  },
 };
