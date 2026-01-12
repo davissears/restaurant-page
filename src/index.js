793ac705e8ce9f569a1e6155a4fd00c2eb916cc3
davissears <davisrandsears@gmail.com>
2026-01-11 18:11:18 -0800

    Initialize app state and rendering logic
    This commit sets up the application's state management and initial
    rendering. It adds an event listener to detect state changes and trigger
    re-rendering of content. It also initializes navigation listeners and
    performs the initial render based on the default application state.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
-// imports
-//  contentHome
-import { State } from "./components/state.js";
-
-import homeBackground from "../images/homeBackground.jpg";
-import aboutBg from "../images/aboutBackground.jpg";
+import { appState } from "./components/state.js";
+import { viewsData } from "./components/views.js";
+import { render } from "./components/content.js";
 import { nav } from "./components/nav.js";
-import { home } from "./components/views.js";
 
-// let appState = new State("home");
-home();
+// Listen for state changes
+appState.addEventListener("statechange", (e) => {
+  const viewName = e.detail.newView;
+  const data = viewsData[viewName];
+  render(data);
+});
+
+// Initialize Navigation Listeners
+nav();
+
+// Initial Render
+render(viewsData[appState.view]);
